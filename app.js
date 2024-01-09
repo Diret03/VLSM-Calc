feb348dff482a38c997ec558eef371a61859e246
Diret03 <diegodavidrecalde@gmail.com>
2024-01-08 22:50:50 -0500

    V1.2
    Mejoras de calidad de vida (UX).

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ class Subnet {
         this.address = '';
         this.prefix = 0;
         this.bitsHost = 0;
-        this.numHosts = 0;
+        this.numRequiredHosts = 0;
         this.allocatedHosts = 0;
         this.minHost = '';
         this.maxHost = '';
@@ -20,37 +20,46 @@ class Subnet {
 }
 
 
-//numero de filas de subredes
+document.getElementById('changeSubnet').addEventListener('click', function (event) {
+    event.preventDefault();
 
-document.getElementById('changeSubnet').addEventListener('click', function () {
-    var numSubnets = document.getElementById('numSubnet').value;
+    let numSubnets = document.getElementById('numSubnet').value;
+    let table = document.getElementById('subnetTable');
 
-    // Clear existing rows
-    var table = document.getElementById('subnetTable');
-    let index = 0;
-    while (table.rows.length > 1) {
-        table.deleteRow(1);
+    // Obtener el numero de filas actuales
+    let currentRows = table.rows.length - 2;
+
+    if (numSubnets <= 0) { //si el numero de subredes solicitado es menor o igual a cero...
+        return; //no hacer nada
+    } else {
+        if (numSubnets == currentRows - 1) { //si el numero de subredes solicitado es igual al numero de filas...
+            return; //no hacer nada
+        } else {
+            if (numSubnets < currentRows) { //si el numero de subredes solicitado es menor al numero actual de filas....
+                let index = currentRows - 1;
+
+                while (table.rows.length - 3 > numSubnets) {  //borrar las filas existentes hasta que su numero sea igual al numero de subredes
+                    table.deleteRow(index);
+                    index--;
+                }
+
+            } else { //si el numero de subredes solicitado es mayor al numero actual de filas....
+
+                for (var i = currentRows; i <= numSubnets; i++) {
+                    let row = table.insertRow(i); //empezar a colocar filas a partir de la ultima fila existente
+                    let cell1 = row.insertCell(0);
+                    let cell2 = row.insertCell(1);
+                    cell1.innerHTML = '<input type="text" name="net_' + i + '" class="name" value="' + String.fromCharCode(65 + i - 1) + '">';
+                    cell2.innerHTML = '<input type="number" name="size_' + i + '" class="size">';
+                }
+            }
+
+        }
     }
-
-    // Generate new rows
-    for (var i = 1; i <= numSubnets; i++) {
-        let row = table.insertRow(-1);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        cell1.innerHTML = '<input type="text" name="net_' + i + '" class="name" value="' + String.fromCharCode(65 + i - 1) + '">';
-        cell2.innerHTML = '<input type="text" name="size_' + i + '" class="size">';
-    }
-
-    let row = table.insertRow(-1);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-
-
-    cell1.innerHTML = '<td>Numero de subredes</td>';
-    cell2.innerHTML = '<td><input type="number" name="" id="numSubnet" value="' + numSubnets + '"><button id="changeSubnet">Cambiar</button></td>';
 
 
 });
+
 
 
 function isValidNetworkFormat(network) {
@@ -75,7 +84,6 @@ function isValidNetworkFormat(network) {
 
     } else {
         isValid = false;
-        alert('Formato inválido de red. Por favor use el formato decimal *.*.*.*');
     }
 
     // Comprobar que solo haya 4 octetos
@@ -90,21 +98,46 @@ function isValidNetworkFormat(network) {
 
 //recolectar hosts
 
-document.getElementById('submit').addEventListener('click', function () {
+document.getElementById('submit').addEventListener('click', function (event) {
 
+    event.preventDefault();
 
     // recuperar red padre
     let majorNetwork = document.getElementById('majorNetwork').value;
     let prefix = document.getElementById('prefix').value;
 
+    var majorNetTD = document.getElementById('majorNetTD');
+    let errorMessage = document.createElement('p');
+    errorMessage.classList.add('error');
+
+    majorNetTD.innerHTML = `
+        <input type="text" name="majorNetwork" id="majorNetwork"
+        placeholder="Ingresar red padre" value='${majorNetwork}'/>
+        <input type="number" name="prefix" id="prefix" placeholder="Prefijo de máscara" value='${prefix}' />
+        `;
+
+
     // Verificar que el formato ingresado de la red sea correcto
     if (!isValidNetworkFormat(majorNetwork)) {
+        // majorNetTD.innerHTML = `
+        // <input type="text" name="majorNetwork" id="majorNetwork"
+        // placeholder="Ingresar red padre" value='${majorNetwork}'/>
+        // <input type="number" name="prefix" id="prefix" placeholder="Prefijo de máscara" value='${prefix}' />
+        // `;
+        majorNetTD.innerHTML += `<br>
+        <p>Formato inválido de red. Por favor use el formato decimal *.*.*.*</p>
+        `;
+        // errorMessage.textContent = 'Formato inválido de red. Por favor use el formato decimal *.*.*.*';
+        // majorNetTD.appendChild(errorMessage);
         return;
     }
 
     // Comprobar que el prefijo sea un número válido entre 1 y 32
     if (isNaN(prefix) || prefix < 1 || prefix > 32) {
-        alert('Prefijo inválido. Por favor use un número entre uno y 32.');
+        majorNetTD.innerHTML += `<br>
+        <p>Prefijo inválido. Por favor use un número entre uno y 32.</p>
+
+        `;
         return;
     }
 
@@ -114,17 +147,29 @@ document.getElementById('submit').addEventListener('click', function () {
     let hostRows = document.querySelectorAll('.name');
     let numHostsRows = document.querySelectorAll('.size');
 
-    // Check if any 'size' input is empty
+
+    let errorRow = document.getElementById('errorRow');
+    let errorMsgP = document.getElementById('errorMessage');
+
+
+    // Comprobar si alguna input de tamano esta vacio 
     let isValid = true;
+    errorMsgP.innerHTML = '';
     numHostsRows.forEach((numHostsInput, index) => {
         let hostName = hostRows[index].value;
         let numHosts = numHostsInput.value;
 
         if (numHosts.trim() === '') {
             isValid = false;
-            alert(`El tamaño del host '${hostName}' no puede estar vacío.`);
+            //errorRow.innerHTML = `<td colspan=2>El tamaño del host '${hostName}' no puede estar vacío.</td>`;
+            errorMsgP.innerHTML += `<td colspan=2>El tamaño del host '${hostName}' no puede estar vacío.</td><br>`;
         } else {
-            hosts.push({ name: hostName, numHosts: numHosts });
+            if (numHosts <= 0) {
+                isValid = false;
+                // errorRow.innerHTML = `<td colspan=2>El host '${hostName}' debe ser mayor a 0.</td>`;
+                errorMsgP.innerHTML += `<td colspan=2>El host '${hostName}' debe ser mayor a 0.</td><br>`;
+            }
+            else hosts.push({ name: hostName, numHosts: numHosts });
         }
     });
 
@@ -145,179 +190,3 @@ document.getElementById('submit').addEventListener('click', function () {
 });
 
 
-//Function para calcular el número de hosts requeridos
-function hostBits(hosts) {
-    let exp = 0;
-
-    while (Math.pow(2, exp) - 2 < hosts) {
-        exp++;
-    }
-
-    return exp;
-}
-
-
-
-//Función recursiva para generar n combinaciones de números binarios
-function generateBinaryCombinations(n) {
-    const arr = [];
-    generateBinaryCombinationsHelper(n, "", arr);
-    return arr;
-}
-
-function generateBinaryCombinationsHelper(n, prefix, arr) {
-    if (n === 0) {
-        arr.push(prefix);
-    } else {
-        generateBinaryCombinationsHelper(n - 1, prefix + "0", arr);
-        generateBinaryCombinationsHelper(n - 1, prefix + "1", arr);
-    }
-}
-
-
-//función para reemplazar una porción de string dentro de otra a partir de un índice inicial
-function replaceSubstring(base, newString, index) {
-    const baseArray = base.split('');
-    const newArray = newString.split('');
-
-    for (let i = 0; i < newString.length;) {
-        if (baseArray[index + i] !== '.') {
-            baseArray[index + i] = newArray[i];
-            i++;
-        } else {
-            index++;
-        }
-    }
-
-    return baseArray.join('');
-}
-
-//función para reemplazar una combinación en binario dentro de una dirección binaria
-function replaceCombination(ipAddress, newString, prefixLength) {
-    const arrStrNoDots = ipAddress.split('.');
-    let strNoDots = '';
-
-    for (let i = 0; i < arrStrNoDots.length; i++) {
-        strNoDots += arrStrNoDots[i];
-    }
-
-    const ans = replaceSubstring(strNoDots, newString, prefixLength - 1);
-    const ipAddressWithDots = [];
-
-    for (let i = 0; i < ans.length; i += 8) {
-        ipAddressWithDots.push(ans.slice(i, i + 8));
-
-        if (i + 8 < ans.length) {
-            ipAddressWithDots.push('.');
-        }
-    }
-
-    return ipAddressWithDots.join('');
-}
-
-
-//función para generar subredes en base a una dirección decimal, su prefijo de mácara y el número de hosts necesarios
-function generateSubnets(majorAdd, prefix, hosts) {
-    let hostBitsValue = hostBits(hosts);
-    let netBits = 32 - prefix - hostBitsValue;
-    if (netBits < 0) {
-
-        return false;
-    } else {
-
-        const bitCombinations = generateBinaryCombinations(netBits);
-
-        const subnets = [];
-
-        let bitToStart = prefix + 1;
-        prefix += netBits;
-
-        for (let i = 0; i < 2 ** netBits; i++) {
-            let ipAddress = addressToBinary(majorAdd);
-
-            let sub = new Subnet();
-            sub.binAddress = replaceCombination(ipAddress, bitCombinations[i], bitToStart);
-            sub.address = binaryToDecimalAddress(sub.binAddress);
-            sub.prefix = prefix;
-            sub.bitsHost = hostBitsValue;
-            sub.numHosts = hosts;
-            sub.allocatedHosts = 2 ** hostBitsValue - 2;
-
-            const hostCombinations = generateBinaryCombinations(sub.bitsHost);
-
-            sub.minHost = binaryToDecimalAddress(replaceCombination(sub.binAddress, hostCombinations[1], prefix + 1));
-            sub.maxHost = binaryToDecimalAddress(replaceCombination(sub.binAddress, hostCombinations[hostCombinations.length - 2], prefix + 1));
-            sub.broadcast = binaryToDecimalAddress(replaceCombination(sub.binAddress, hostCombinations[hostCombinations.length - 1], prefix + 1));
-
-            subnets.push(sub);
-        }
-
-        return subnets;
-    }
-
-}
-
-//función para generar subredes en base a una dirección decimal, su prefijo de mácara y un arreglo de hosts ordenados de mayor a menor
-function finalSubnets(majorAdd, prefix, hosts) {
-
-    let index = 0;
-    const results = [];
-    const unasignedSubnets = [];
-
-    while (results.length < hosts.length) {
-        let subnetting = null;
-
-        const numHosts = hosts[index].numHosts;
-
-        if (!results.length) {
-            subnetting = generateSubnets(majorAdd, prefix, numHosts);
-            if (!subnetting) {
-
-                return false;
-            }
-
-        } else {
-            if (unasignedSubnets.length) {
-                let auxSubnet = unasignedSubnets[0];
-                subnetting = generateSubnets(auxSubnet.address, auxSubnet.prefix, numHosts);
-                unasignedSubnets.shift();
-            } else {
-                results.push(null);
-                break;
-            }
-        }
-
-        for (let i = 0; i < subnetting.length; i++) {
-            let current = subnetting[i];
-            let numBitsHosts = current.bitsHost;
-
-            if (index < hosts.length && 2 ** numBitsHosts - 2 >= hosts[index].numHosts) {
-                let wasNumBitsChanged = false;
-
-                while (2 ** numBitsHosts - 2 > hosts[index].numHosts) {
-                    if (2 ** (numBitsHosts - 1) - 2 > hosts[index].numHosts) {
-                        numBitsHosts--;
-                        wasNumBitsChanged = true;
-                    } else {
-                        break;
-                    }
-                }
-
-                if (!wasNumBitsChanged) {
-                    current.name = hosts[index].name;
-                    results.push(current);
-                    index++;
-                } else {
-                    if (unasignedSubnets.length === 0) {
-                        unasignedSubnets.push(...subnetting.slice(i));
-                    } else {
-                        unasignedSubnets.unshift(current);
-                    }
-                    break;
-                }
-            }
-        }
-    }
-
-    return results;
-}
--- subnetting.js
+++ subnetting.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const prefix = localStorage.getItem('prefix');
     const hosts = JSON.parse(localStorage.getItem('hosts'));
 
-    // Ensure that the required data is available
+    // Asegurarse que la informacion se recolecto correctamente
     if (majorNetwork && prefix && hosts) {
         // Call a function to generate and populate the table
 
@@ -54,7 +54,21 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-
+// Funcion para añadir una fila a la tabla
+function addRow(subnet) {
+    const tableBody = document.querySelector('#subnetTable tbody');
+    const row = tableBody.insertRow();
+    row.insertCell(0).textContent = subnet.name;
+    row.insertCell(1).textContent = subnet.numRequiredHosts;
+    row.insertCell(2).textContent = subnet.allocatedHosts;
+    row.insertCell(3).textContent = subnet.binAddress;
+    row.insertCell(4).textContent = subnet.address;
+    row.insertCell(5).textContent = `/${subnet.prefix}`;
+    row.insertCell(6).textContent = subnetMasks[subnet.prefix];
+    row.insertCell(7).textContent = subnet.minHost;
+    row.insertCell(8).textContent = subnet.maxHost;
+    row.insertCell(9).textContent = subnet.broadcast;
+}
 
 function generateSubnetTable(majorNetwork, prefix, hosts) {
     // Get the table body
@@ -63,20 +77,7 @@ function generateSubnetTable(majorNetwork, prefix, hosts) {
     // Clear existing rows
     tableBody.innerHTML = '';
 
-    // Function to add a row to the table
-    function addRow(subnet) {
-        const row = tableBody.insertRow();
-        row.insertCell(0).textContent = subnet.name;
-        row.insertCell(1).textContent = subnet.numHosts;
-        row.insertCell(2).textContent = subnet.allocatedHosts;
-        row.insertCell(3).textContent = subnet.binAddress;
-        row.insertCell(4).textContent = subnet.address;
-        row.insertCell(5).textContent = `/${subnet.prefix}`;
-        row.insertCell(6).textContent = subnetMasks[subnet.prefix];
-        row.insertCell(7).textContent = subnet.minHost;
-        row.insertCell(8).textContent = subnet.maxHost;
-        row.insertCell(9).textContent = subnet.broadcast;
-    }
+
 
     // Your logic to generate subnets based on majorNetwork, prefix, and hosts
     const subnets = finalSubnets(majorNetwork, prefix, hosts);
@@ -88,9 +89,192 @@ function generateSubnetTable(majorNetwork, prefix, hosts) {
         return false;
     }
 
-    // Populate the table with generated subnets
+    //Llenar la tabla con las subredes generadas
     subnets.forEach(subnet => {
         addRow(subnet);
     });
 }
 
+//Function para calcular el número de hosts requeridos
+function hostBits(hosts) {
+    let exp = 0;
+
+    while (Math.pow(2, exp) - 2 < hosts) {
+        exp++;
+    }
+
+    return exp;
+}
+
+
+
+//Función recursiva para generar n combinaciones de números binarios
+function generateBinaryCombinations(n) {
+    const arr = [];
+    generateBinaryCombinationsHelper(n, "", arr);
+    return arr;
+}
+
+function generateBinaryCombinationsHelper(n, prefix, arr) {
+    if (n === 0) {
+        arr.push(prefix);
+    } else {
+        generateBinaryCombinationsHelper(n - 1, prefix + "0", arr);
+        generateBinaryCombinationsHelper(n - 1, prefix + "1", arr);
+    }
+}
+
+
+//función para reemplazar una porción de string dentro de otra a partir de un índice inicial
+function replaceSubstring(base, newString, index) {
+    const baseArray = base.split('');
+    const newArray = newString.split('');
+
+    for (let i = 0; i < newString.length;) {
+        if (baseArray[index + i] !== '.') {
+            baseArray[index + i] = newArray[i];
+            i++;
+        } else {
+            index++;
+        }
+    }
+
+    return baseArray.join('');
+}
+
+//función para reemplazar una combinación en binario dentro de una dirección binaria
+function replaceCombination(ipAddress, newString, prefixLength) {
+    const arrStrNoDots = ipAddress.split('.');
+    let strNoDots = '';
+
+    for (let i = 0; i < arrStrNoDots.length; i++) {
+        strNoDots += arrStrNoDots[i];
+    }
+
+    const ans = replaceSubstring(strNoDots, newString, prefixLength - 1);
+    const ipAddressWithDots = [];
+
+    for (let i = 0; i < ans.length; i += 8) {
+        ipAddressWithDots.push(ans.slice(i, i + 8));
+
+        if (i + 8 < ans.length) {
+            ipAddressWithDots.push('.');
+        }
+    }
+
+    return ipAddressWithDots.join('');
+}
+
+
+//función para generar subredes en base a una dirección decimal, su prefijo de mácara y el número de hosts necesarios
+function generateSubnets(majorAdd, prefix, hosts) {
+    let hostBitsValue = hostBits(hosts);
+    let netBits = 32 - prefix - hostBitsValue;
+    if (netBits < 0) {
+
+        return false;
+    } else {
+
+
+
+        const bitCombinations = generateBinaryCombinations(netBits);
+
+        const subnets = [];
+
+        let bitToStart = prefix + 1;
+        prefix += netBits;
+
+        for (let i = 0; i < 2 ** netBits; i++) {
+            let ipAddress = addressToBinary(majorAdd);
+
+            let sub = new Subnet();
+            sub.binAddress = replaceCombination(ipAddress, bitCombinations[i], bitToStart);
+            sub.address = binaryToDecimalAddress(sub.binAddress);
+            sub.prefix = prefix;
+            sub.bitsHost = hostBitsValue;
+            sub.numRequiredHosts = parseInt(hosts);
+            sub.allocatedHosts = 2 ** hostBitsValue - 2;
+
+            // if (sub.allocatedHosts > sub.ne) {
+
+            // }
+            const hostCombinations = generateBinaryCombinations(sub.bitsHost);
+
+            sub.minHost = binaryToDecimalAddress(replaceCombination(sub.binAddress, hostCombinations[1], prefix + 1));
+            sub.maxHost = binaryToDecimalAddress(replaceCombination(sub.binAddress, hostCombinations[hostCombinations.length - 2], prefix + 1));
+            sub.broadcast = binaryToDecimalAddress(replaceCombination(sub.binAddress, hostCombinations[hostCombinations.length - 1], prefix + 1));
+
+            subnets.push(sub);
+        }
+
+        return subnets;
+    }
+
+}
+
+//función para generar subredes en base a una dirección decimal, su prefijo de mácara y un arreglo de hosts ordenados de mayor a menor
+function finalSubnets(majorAdd, prefix, hosts) {
+
+    let index = 0;
+    const results = [];
+    const unasignedSubnets = [];
+
+    while (results.length < hosts.length) {
+        let subnetting = null;
+
+        const numHosts = hosts[index].numHosts;
+
+        if (!results.length) {
+            subnetting = generateSubnets(majorAdd, prefix, numHosts);
+            if (!subnetting) {
+
+                return false;
+            }
+
+        } else {
+            if (unasignedSubnets.length) {
+                let auxSubnet = unasignedSubnets[0];
+                subnetting = generateSubnets(auxSubnet.address, auxSubnet.prefix, numHosts);
+                unasignedSubnets.shift();
+
+            } else {
+                results.push(null);
+                break;
+            }
+        }
+
+        for (let i = 0; i < subnetting.length; i++) {
+            let current = subnetting[i];
+            let numBitsHosts = current.bitsHost;
+
+            if (index < hosts.length && 2 ** numBitsHosts - 2 >= hosts[index].numHosts) {
+                let wasNumBitsChanged = false;
+
+                while (2 ** numBitsHosts - 2 > hosts[index].numHosts) {
+                    if (2 ** (numBitsHosts - 1) - 2 > hosts[index].numHosts) {   //asegurarse de no desperdiciar hosts con el tamaño actual
+                        numBitsHosts--;
+                        wasNumBitsChanged = true;
+                    } else {
+                        break;  //romper ciclo cuando se tenga la cantidad de bits de hosts optima
+                    }
+                }
+
+                if (!wasNumBitsChanged) {
+                    current.name = hosts[index].name;
+                    results.push(current);
+                    index++;
+                } else {
+                    if (unasignedSubnets.length === 0) {
+                        unasignedSubnets.push(...subnetting.slice(i));
+                    } else {
+                        unasignedSubnets.unshift(current);
+                    }
+                    break;
+                }
+            }
+            //else break;
+        }
+    }
+
+    return results;
+}
